1b9cabfeac8cbf46235c013c4fbee04a6e7aac6d
wormhole <abracadabra2020@163.com>
2021-05-12 17:17:30 +0800

    add rem adapter

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,19 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
+
+<script>
+//得到手机屏幕的宽度
+let htmlWidth =
+  document.documentElement.clientWidth || document.body.clientWidth;
+//得到html的Dom元素
+let htmlDom = document.getElementsByTagName("html")[0];
+//设置根元素字体大小
+htmlDom.style.fontSize = htmlWidth / 20 + "px";
+</script>
 
 <style lang="scss">
 #app {
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,5 +2,29 @@
   <div class="about">
     <h1>This is an about page</h1>
     <van-button type="primary">主要按钮</van-button>
+    <div class="test">
+      <p class="hello">Hello jspang</p>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.test {
+  width: 320px;
+  height: 160px;
+  background-color: yellowgreen;
+  text-align: text;
+  margin: 0 auto;
+  .hello {
+    color: rgb(90, 12, 25);
+    font-size: 16px;
+  }
+}
+.test {
+  width: 20rem;
+  height: 10rem;
+  .hello {
+    font-size: 1rem;
+  }
+}
+</style>
